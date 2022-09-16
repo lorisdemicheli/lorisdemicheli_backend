3bd644ab1220dcb40c0ca77a7d7429de3c3a5bcf
lorisdemicheli <lorisdemicheli@gmail.com>
2022-09-16 17:11:56 +0200

    add forst part user

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,30 @@ let sql = require('../util/sql');
 
 
 router.get('/:userId', async (req, res) => {
-  let sop = await sql.query('SELECT * FROM sites_user WHERE username = ?', [req.params.userId]);
-  res.json(sop);
+  let sqlRes = await sql.query('SELECT * FROM sites_user WHERE username = ?', [req.params.userId]);
+  res.json(sqlRes);
+});
+
+router.get('/:userId/match', async (req, res) => {
+  let sqlRes = await sql.query('SELECT m.* FROM sites_user u \
+                                LEFT JOIN sites_encounters e ON e.user_id = u.id OR e.user_match_id = u.id \
+                                LEFT JOIN sites_user m ON m.id = if(u.id = e.user_id, e.user_match_id, e.user_id)\
+                                WHERE u.username = ?', [req.params.userId]);
+  res.json(sqlRes);
+});
+
+router.get('/:userId/qrcode', async (req, res) => {
+  const qrQuery = 'SELECT qr.code FROM sites_user u \
+                    LEFT JOIN sites_qr_code qr ON qr.user_id = u.id\
+                    WHERE u.username = ? \
+                    AND date_add(qr.creation_date,interval 5 minute) > now()';
+  let sqlRes = await sql.query(qrQuery, [req.params.userId]);
+  if(sqlRes.length < 1) {
+    await sql.query('INSERT INTO sites_qr_code (user_id,creation_date,code) \
+                      (SELECT u.id,now(),UUID() FROM sites_user u WHERE u.username = ?)', [req.params.userId]);
+    sqlRes = await sql.query(qrQuery, [req.params.userId]);
+  } 
+  res.json(sqlRes);
 });
 
 module.exports = router;
---
+++ sql/20220916.sql
@@ -0,0 +1,28 @@
+CREATE TABLE sites_user (
+    id int AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    google_id varchar(25) NOT NULL,
+    url_image varchar(255) DEFAULT NULL,
+    birth_date datetime DEFAULT NULL,
+    CONSTRAINT PK_user PRIMARY KEY (id)
+);
+
+CREATE TABLE sites_encounters (
+    id int AUTO_INCREMENT,
+    user_id int NOT NULL,
+    user_match_id int NOT NULL,
+    creation_date datetime DEFAULT NULL,
+    CONSTRAINT PK_user PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES sites_user(id),
+    FOREIGN KEY (user_match_id) REFERENCES sites_user(id)
+);
+
+
+CREATE TABLE sites_qr_code (
+    id int AUTO_INCREMENT,
+    user_id int NOT NULL,
+    creation_date datetime DEFAULT now(),
+    code varchar(255) NOT NULL,
+    CONSTRAINT PK_user PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES sites_user(id)
+);
