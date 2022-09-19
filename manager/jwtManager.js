dd7676ec7a6433de637fa6cbe6e0fb8f69c0dee0
lorisdemicheli <lorisdemicheli@gmail.com>
2022-09-19 15:10:44 +0200

    add log

--- manager/jwtManager.js
+++ manager/jwtManager.js
@@ -6,6 +6,8 @@ module.exports.checkAuthorization = function (req, res, next) {
         const bearerToken = bearerHeader.split(' ')[1];
         jwt.verify(bearerToken, process.env.JWT_KEY, (err, authData) => {
             if (err) {
+                console.log(req);
+                console.log(err);
                 res.sendStatus(403);
             } else {
                 req.auth = authData;
